d480a7294492272b3d1dd684e263fc57b5fa88fb
jsmarra <jsmarra@users.noreply.github.com>
2015-03-21 18:40:56 -0400

    Update cachematrix.R
    changed the comments in the code above
    testing out the concept of putting comments when i commit code

--- cachematrix.R
+++ cachematrix.R
@@ -19,7 +19,6 @@ makeCacheMatrix <- function(x = matrix()) {
 ## returning a matrix that is the inverse of the matrix supplied
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
   inv <- x$getinverse()
   if(!is.null(inv)) {
     message("getting cached data.")
